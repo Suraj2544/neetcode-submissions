1b104d6a2998b2b33e0310b77ed2e2c6ea0d3f61
suraj mishra <97424731+Suraj2544@users.noreply.github.com>
2026-05-19 13:08:51 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Set<Integer> seen = new HashSet<>();
+
+            for (int j = i + 1; j < nums.length; j++) {
+                int complement = -nums[i] - nums[j];
+
+                if (seen.contains(complement)) {
+                    result.add(Arrays.asList(nums[i], complement, nums[j]));
+                }
+                seen.add(nums[j]);
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
+}
